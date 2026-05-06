c5c6fb3734f09414c79cfdec79071acedaa85ede
Alberto Ito <ito.alberto.88@gmail.com>
2026-05-06 12:45:51 +0000

    Fix firebase integration
    # Conflicts:
    #	01_simple_google_sso/swift/.DS_Store

---
+++ 02_swift_common_ui/swift/common_ui/common_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  common_uiApp.swift
+//  common_ui
+//
+//  Created by ec2-user on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct common_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
