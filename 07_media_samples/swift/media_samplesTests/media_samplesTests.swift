e0da51ae65c6b2755d85b381e677ee58d9bc3fc3
Alberto Ito <ito.alberto.88@gmail.com>
2026-05-20 14:44:51 +0000

    Fix locaization and add media samples app

---
+++ 07_media_samples/swift/media_samplesTests/media_samplesTests.swift
@@ -0,0 +1,19 @@
+//
+//  media_samplesTests.swift
+//  media_samplesTests
+//
+//  Created by ec2-user on 5/20/26.
+//
+
+import Testing
+@testable import media_samples
+
+struct media_samplesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
